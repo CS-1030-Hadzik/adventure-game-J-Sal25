71b61f66eba4e4d2c1a38443e4f0edbb81b97b54
Josh Salazar <josuesalazar@mail.weber.edu>
2025-01-16 09:35:08 -0700

    ADD: Author Information

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
+# welcome messange and introduction 
 print("Welcome to the Adventure Game!")
 print("Your journey begins here...") 
+
+# ask for the players name 
+player_name = input ("what is your name, adventurer?")
+
+#Concatenate strings to create a personalized message 
+print ("Welcome, " + player_name + "! Your journey begins now.") 
