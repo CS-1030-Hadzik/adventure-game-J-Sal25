888cc1c392b774f751e1f53f9bbba2b0cf309021
Josh Salazar <josuesalazar@mail.weber.edu>
2025-01-28 10:11:21 -0700

    Added Extra notes to code. Code has been completed

--- game.py
+++ game.py
@@ -1,5 +1,13 @@
-# welcome messange and introduction
-# THis is a text based adventure game  
+''' 
+This is a dosctring, a type of comment that isnt actually run in the code, Can be used with double quots "" or single quotes if its three in a row '
+Adventure Game 
+Author: Josue Salazar
+Version 1.0 
+Description: This is a text-based adventure game 
+ This is a text based adventure game where the player makes choices to navigate through a mysterious forest.
+ '''
+ # the hashtag is used mainly for comments, same use as the quotation marks
+
 '''Adventure Game 
 Author: Josue Salazar 
 Version : 1.0
@@ -11,7 +19,7 @@ print("Your journey begins here...")
 # ask for the players name 
 player_name = input ("what is your name, adventurer?")
 
-#Concatenate strings to create a personalized message 
+#Concatenate(combining) strings to create a personalized message 
 print ("Welcome, " + player_name + "! Your journey begins now.") 
 
 #use an f-string to display the same message in a more readable way 
@@ -19,9 +27,7 @@ print ("Welcome, " + player_name + "! Your journey begins now.")
 print (f"Welcome, {player_name}! Your journey begins now.")
 
 #describe the starting area 
-starting_area = """"
-You Find yourself in a dark forest. The sound of rustling leaves fills the air. A faint path lies ahead, leading deeper into the unknown...
-"""
+starting_area = """" You Find yourself in a dark forest. The sound of rustling leaves fills the air. A faint path lies ahead, leading deeper into the unknown..."""
 print (starting_area)
 
 #ask the player for their first decison
@@ -32,5 +38,5 @@ decision = input ("Do you wish to take the path? (yes or no): ").lower ()
 if decision == "yes":
     print(f"Brave choice, {player_name}! You step onto the path and venture forward.")
 elif decision == "no":
-    print (player_name + ", you decide to wait. Perhaps courage will find you later.") #concatenation exapmple
+    print (player_name + ", you decide to wait. Perhaps courage will find you later.") #concatenation example
 else: print ("Confused, you stand still, unsure of what to do")
