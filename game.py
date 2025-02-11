588830b74ebf24b0b2d4530854d1926abe87d2d3
Josh Salazar <josuesalazar@mail.weber.edu>
2025-02-11 09:56:05 -0700

    REF: infinite loop for game choice

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ print("Welcome to the Adventure Game!")
 print("Your journey begins here...") 
 
 # ask for the players name 
-player_name = input ("what is your name, adventurer?")
+player_name = input ("what is your name, adventurer? ")
 
 #Concatenate(combining) strings to create a personalized message 
 print ("Welcome, " + player_name + "! Your journey begins now.") 
@@ -30,20 +30,24 @@ print (f"Welcome, {player_name}! Your journey begins now.")
 starting_area = """" You Find yourself in a dark forest. The sound of rustling leaves fills the air. A faint path lies ahead, leading deeper into the unknown..."""
 print (starting_area)
 
-#ask the player for their first decison
-decision = input("Do you wish to take the path? (yes or no): ").lower ()
+#start the game loop
+while True: 
+    print ("\nYou see two paths ahead:")
+    print ("\t1. Take the left path into the dark woods.")
+    print ("\t2. Take the right path towards the mountain pass.") 
+    print ("\t3. Stay where you are.") 
+
+    decision = input("What will you do (1,2,3): ") 
+
+    if decision == "1": 
+        print (f"{player_name}, you step into the dark woods. The trees whisper as you walk deeper. ") 
+    elif decision == "2": 
+        print (f"{player_name}, you make your way towards the mountain pass feeling the cold wind against your face.")
+    elif decision == "3":
+        print ("You stay still, listening to the distant sounds of the forest.")
+    else:
+        print("invalid choice, please choose 1, 2, or 3. ")
 
 
-#invalid reponse loop uintil they give a valid response
-while decision not in ["yes", "no"]:
-    print("invalid choice. PLease type 'yes' or 'no'.")
-    #option for the user to make a new decision
-    decision = input("Do you wish to take the path (yes or no):").lower()
-
-#respond based on the players decison 
-if decision == "yes":
-    print(f"Brave choice, {player_name}! You step onto the path and venture forward.")
-elif decision == "no":
-    print (player_name + ", you decide to wait. Perhaps courage will find you later.") #concatenation example
 
 
