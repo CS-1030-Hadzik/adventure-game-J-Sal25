54865dc19919d89edc5c1ddcbc36dcf975dd1b17
Josh Salazar <josuesalazar@mail.weber.edu>
2025-04-10 09:17:08 -0600

    COMPLETE: FInal game done!

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.inventory = []
-        self.health = 100
+        self.health = 100 #TODO change this back to 100 
         self.has_map = False
         self.has_lantern = False
 
